1280a3b6d91912e2a9263b9e7ed68c5fd57d6fef
Nicolas Maupu <nmaupu@gmail.com>
2012-02-06 21:27:10 +0100

    Initialize database with some default data if empty (first launch for example)

--- src/org/maupu/android/tmh/HomeActivity.java
+++ src/org/maupu/android/tmh/HomeActivity.java
@@ -1,15 +1,25 @@
 package org.maupu.android.tmh;
 
+import java.util.Date;
+
 import org.maupu.android.tmh.database.DatabaseHelper;
+import org.maupu.android.tmh.database.object.Account;
+import org.maupu.android.tmh.database.object.Category;
+import org.maupu.android.tmh.database.object.Currency;
 
 import android.app.TabActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Window;
 import android.widget.TabHost;
 
 public class HomeActivity extends TabActivity {
+	private static String PREF_KEY_APP_INIT;
+	
 	private DatabaseHelper dbHelper = new DatabaseHelper(this);
 	//private Account selectedAccount;
 
@@ -23,6 +33,9 @@ public class HomeActivity extends TabActivity {
 		//customTB.setIcon(R.drawable.ic_stat_categories);
 
 		dbHelper.openWritable();
+		
+		// initialize if needed
+		init();
 				
 		addTab(ViewPagerOperationActivity.class, R.string.tab_operation, R.drawable.ic_tab_home);
 		//addTab(ManageOperationActivity.class, R.string.tab_credit, R.drawable.ic_tab_credit);
@@ -41,5 +54,45 @@ public class HomeActivity extends TabActivity {
 		spec = tabHost.newTabSpec(getString(titleId)).setIndicator(getString(titleId),
 				res.getDrawable(drawableId)).setContent(intent);
 		tabHost.addTab(spec);
+	}
+	
+	/**
+	 * Called when app init. Creates required database objects and show first launch activity
+	 */
+	private void init() {
+		//SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		//boolean isAlreadyInit = prefs.getBoolean(PREF_KEY_APP_INIT, false);
+		Account account = new Account();
+		int nb = account.getCount(dbHelper);
+		
+		//if(! isAlreadyInit || nb == 0) {
+		if(nb == 0) {
+			// initialization
+			// Create default category
+			Category category = new Category();
+			category.setName(getString(R.string.withdrawal));
+			category.insert(dbHelper);
+			
+			// Adding default currency (euro)
+			Currency currency = new Currency();
+			currency.setIsoCode("EUR");
+			currency.setLastUpdate(new Date());
+			currency.setLongName("Euro");
+			java.util.Currency c = java.util.Currency.getInstance("EUR");
+			currency.setShortName(c.getSymbol());
+			currency.setTauxEuro(1.0f);
+			currency.insert(dbHelper);
+			
+			// Creating a default account in euro
+			account.setCurrency(currency);
+			account.setName(getString(R.string.default_account_name));
+			account.insert(dbHelper);
+			
+			/*
+			Editor editor = prefs.edit();
+			editor.putBoolean(PREF_KEY_APP_INIT, true);
+			editor.commit();
+			*/
+		}
 	}
 }
