17434163671324be30f5884b34bf6863c3d97c1d
nmaupu <nmaupu@gmail.com>
2015-11-16 01:16:39 +0100

    Cleaning

--- travelerMoneyHelper/src/main/java/org/maupu/android/tmh/ui/TmhNavigationDrawerClickListener.java
+++ travelerMoneyHelper/src/main/java/org/maupu/android/tmh/ui/TmhNavigationDrawerClickListener.java
@@ -1,11 +1,14 @@
 package org.maupu.android.tmh.ui;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import org.maupu.android.tmh.StatsActivity;
+import org.maupu.android.tmh.ViewPagerOperationActivity;
 import org.maupu.android.tmh.ui.widget.IconArrayAdapter;
 
 public class TmhNavigationDrawerClickListener implements AdapterView.OnItemClickListener {
@@ -19,8 +22,17 @@ public class TmhNavigationDrawerClickListener implements AdapterView.OnItemClick
 
     @Override
     public void onItemClick(AdapterView adapterView, View view, int position, long id) {
-        Log.d(TmhNavigationDrawerClickListener.class.getName(), "onItemClicked : "+adapterView.getAdapter().getItem(position));
+        Log.d(TmhNavigationDrawerClickListener.class.getName(), "onItemClicked : " + adapterView.getAdapter().getItem(position));
         ((IconArrayAdapter)adapterView.getAdapter()).selectItem(position);
+        /*
+        switch(position) {
+            case 0:
+                adapterView.getContext().startActivity(new Intent(adapterView.getContext(), ViewPagerOperationActivity.class));
+                break;
+            case 1:
+                adapterView.getContext().startActivity(new Intent(adapterView.getContext(), StatsActivity.class));
+                break;
+        }*/
         this.drawerLayout.closeDrawer(drawerList);
     }
 }
