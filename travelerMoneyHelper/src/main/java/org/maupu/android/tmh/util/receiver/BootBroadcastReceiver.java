f2fb3b4c109222701cf1c8f175a5d9a492a352e3
Nicolas Maupu <nmaupu@gmail.com>
2023-01-02 13:20:15 +0100

    Fix bug display backup next schedule notification

--- travelerMoneyHelper/src/main/java/org/maupu/android/tmh/util/receiver/BootBroadcastReceiver.java
+++ travelerMoneyHelper/src/main/java/org/maupu/android/tmh/util/receiver/BootBroadcastReceiver.java
@@ -21,12 +21,17 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
         amh.registerDriveBackupAlarm();
         TmhApplication.alarmManagerHelper = amh;
 
-        if (StaticData.PREF_DRIVE_AUTOMATIC_BACKUP_FREQ_NEVER != StaticData.getPreferenceValueInt(StaticData.PREF_DRIVE_AUTOMATIC_BACKUP_FREQ_KEY)
+        String nextAlarm = amh.getNextAlarmDateTimeAsString();
+
+        if (!"".equals(nextAlarm)
+                && StaticData.getPreferenceValueBoolean(StaticData.PREF_KEY_DRIVE_ACTIVATE)
+                && StaticData.PREF_DRIVE_AUTOMATIC_BACKUP_FREQ_NEVER != StaticData.getPreferenceValueInt(StaticData.PREF_DRIVE_AUTOMATIC_BACKUP_FREQ_KEY)
                 && StaticData.getPreferenceValueBoolean(StaticData.PREF_KEY_DRIVE_AUTOMATIC_BACKUP_BOOT_NOTIFICATION)) {
+
             NotificationCompat.Builder builder = NotificationHelper.getNewNotificationBuilder(context, notificationChannelId, R.drawable.ic_baseline_backup_black_24);
             builder
                     .setContentTitle(context.getString(R.string.backup_boot_notification_title))
-                    .setContentText(amh.getNextAlarmDateTimeAsString());
+                    .setContentText(nextAlarm);
             NotificationHelper.getNotificationManager(context).notify(NotificationHelper.getRandomNotificationId(), builder.build());
         }
     }
